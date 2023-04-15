56910b5319e940701f9202ad27391d3be71e74bc
xDShiro_Home <cmex-d@yandex.ru>
2023-04-14 22:09:03 -0400

    coefficients for optimized (not final probably)

--- presentation_plots_2023.py
+++ presentation_plots_2023.py
@@ -2,8 +2,8 @@ from functions_based import *
 import my_functions.beams_and_pulses as bp
 import my_functions.plotings as pl
 
-x_lim_3D, y_lim_3D, z_lim_3D = (-4, 4), (-4, 4), (-1.0, 1.0)
-res_x_3D, res_y_3D, res_z_3D = 270, 270, 3
+x_lim_3D, y_lim_3D, z_lim_3D = (-2.5, 2.5), (-2.5, 2.5), (-0.7, 0.7)
+res_x_3D, res_y_3D, res_z_3D = 71, 71, 71
 x_3D = np.linspace(*x_lim_3D, res_x_3D)
 y_3D = np.linspace(*y_lim_3D, res_y_3D)
 z_3D = np.linspace(*z_lim_3D, res_z_3D)
@@ -36,7 +36,7 @@ if False:
     plot_field(field_edge, axes=False)
     plt.show()
 # LG
-if 1:
+if 0:
     field = bp.LG_simple(*mesh_2D, l=3, p=0)
     plot_field(field/field.max(), axes=False, cmap='viridis')
     plt.show()
@@ -78,17 +78,24 @@ if 0:
     plt.show()
 
 # trefoil 3D
-if 0:
+if 1:
     C00 = 1.51
     C01 = -5.06
     C02 = 7.23
     C03 = -2.04
     C30 = -3.97
+    # normal
     C00 = 1.71
     C01 = -5.66
     C02 = 6.38
     C03 = -2.3
     C30 = -4.36
+    # our
+    C00 = 1.55
+    C01 = -5.11
+    C02 = 8.29
+    C03 = -2.37
+    C30 = -5.36
     field = (
             C00 * bp.LG_simple(*mesh_3D, l=0, p=0) +
             C01 * bp.LG_simple(*mesh_3D, l=0, p=1) +
@@ -100,10 +107,10 @@ if 0:
     plot_field(field, axes=False)
     plt.show()
     # exit()
-    Fig = pl.plot_3D_density(np.abs(field), mesh=mesh_3D_res, show=False, opacity=0.15,
-                             opacityscale='max', colorscale='Jet')
+    # Fig = pl.plot_3D_density(np.abs(field), mesh=mesh_3D_res, show=False, opacity=0.15,
+    #                          opacityscale='max', colorscale='Jet')
     _, dots_init = sing.get_singularities(np.angle(field), axesAll=True, returnDict=True)
-    dp.plotDots(dots_init, boundary_3D, color='black', show=True, size=10, fig=Fig)
+    dp.plotDots(dots_init, boundary_3D, color='black', show=True, size=10)  #, fig=Fig)
     plt.show()
 
 # trefoil polynomials
@@ -132,7 +139,7 @@ if 0:
     plt.show()
     
 # trefoil polynomials modifications
-if 1:
+if 0:
     C00 = 1.71
     C01 = -5.66
     C02 = 6.38
